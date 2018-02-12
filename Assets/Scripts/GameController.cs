08e38883bccfa4df8c32400dbc51742d126165e2
tommy-tran <trant28@mcmaster.ca>
2018-02-12 03:34:19 -0500

    Assets, simple spawning script, player movement

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject enemy;
+    public Vector3 spawnHorizontal;
+    public int numEnemies;
+
+    void Start()
+    {
+        StartWave();
+    }
+
+    void StartWave()
+    {
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            int side = Random.Range(0, 2);
+            print(side);
+            Vector3 spawnPosition;
+            if (side == 0)
+            {
+                spawnPosition = new Vector3(-9, 0, Random.Range(-spawnHorizontal.z, spawnHorizontal.z));
+            }
+            else
+            {
+                spawnPosition = new Vector3(9, 0, Random.Range(-spawnHorizontal.z, spawnHorizontal.z));
+            }
+            Quaternion spawnRotation = new Quaternion();
+            GameObject spawnedEnemy = Instantiate(enemy, spawnPosition, spawnRotation);
+            
+            yield return new WaitForSeconds(3);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float runSpeed = 5f;
+    public float walkSpeed = 1f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = Input.GetButton("Shift") ? movement.normalized * walkSpeed * Time.deltaTime : movement.normalized * runSpeed * Time.deltaTime;
+        if (movement != Vector3.zero)
+        {
+            playerRigidbody.MovePosition(transform.position + movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool IsWalking = Input.GetButton("Shift");
+        bool IsMoving = h != 0f || v != 0f;
+        
+        anim.SetBool("IsWalking", IsWalking);
+        anim.SetBool("IsMoving", IsMoving);
+    }
+
+}
