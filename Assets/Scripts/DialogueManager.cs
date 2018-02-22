45655bd3dfcabe9c5ab053c0902e64b2b9a03e06
tommy-tran <trant28@mcmaster.ca>
2018-02-22 05:11:08 -0500

    Level and dialogue transitions

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+    public Animator anim;
+    private bool state; // True when dialogue box is open
+
+	void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        anim.SetBool("isOpen", true);
+        state = true;
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines(); // when user continues while sentence is animating
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+
+    void EndDialogue()
+    {
+        anim.SetBool("isOpen", false);
+        Debug.Log("End of conversation");
+        state = false;
+    }
+
+    public bool getState()
+    {
+        return state;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public SpawnController spawner;
+    public DialogueManager dialogueManager;
+    public DialogueTrigger[] dialogueTrigger;
+    int level;
 
     void Start()
     {
+        level = 0;
+        dialogueManager.StartDialogue(dialogueTrigger[level].dialogue);
+        StartCoroutine(waitForDialogue());
+    }
+
+    IEnumerator waitForDialogue()
+    {
+        yield return new WaitUntil(() => !dialogueManager.getState());
+        Debug.Log("Finished dialogue");
+        yield return new WaitForSeconds(2f);
         spawnWave();
+        StartCoroutine(waitForWave());
+    }
+
+    IEnumerator waitForWave()
+    {
+        yield return new WaitUntil(() => GameObject.FindWithTag("Enemy") == null);
+        yield return new WaitForSeconds(2f);
+        dialogueManager.StartDialogue(dialogueTrigger[level].dialogue);
+        StartCoroutine(waitForDialogue());
     }
 
     void spawnWave()
     {
-        spawner.StartWave(0);
-    }
-
-    void FixedUpdate()
-    {
-        
+        spawner.StartWave(level++);
     }
 
     void GameOver()
